282971e7068d94ceab55142d9aa5ec744d85b445
Zuhaib Asif <zohaibs198@gmail.com>
2024-07-08 23:39:22 +0500

    Font and Custom CSS Enviornment Set

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,33 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        gilroy: {
+          black: "Gilroy-Black, sans-serif",
+          blackItalic: "Gilroy-BlackItalic, sans-serif",
+          bold: "Gilroy-Bold, sans-serif",
+          boldItalic: "Gilroy-BoldItalic, sans-serif",
+          extraBold: "Gilroy-ExtraBold, sans-serif",
+          extraBoldItalic: "Gilroy-ExtraBoldItalic, sans-serif",
+          heavy: "Gilroy-Heavy, sans-serif",
+          heavyItalic: "Gilroy-HeavyItalic, sans-serif",
+          light: "Gilroy-Light, sans-serif",
+          lightItalic: "Gilroy-LightItalic, sans-serif",
+          medium: "Gilroy-Medium, sans-serif",
+          mediumItalic: "Gilroy-MediumItalic, sans-serif",
+          regular: "Gilroy-Regular, sans-serif",
+          regularItalic: "Gilroy-RegularItalic, sans-serif",
+          semiBold: "Gilroy-SemiBold, sans-serif",
+          semiBoldItalic: "Gilroy-SemiBoldItalic, sans-serif",
+          thin: "Gilroy-Thin, sans-serif",
+          thinItalic: "Gilroy-ThinItalic, sans-serif",
+          ultraLight: "Gilroy-UltraLight, sans-serif",
+          ultraLightItalic: "Gilroy-UltraLightItalic, sans-serif",
+        },
+        swanky: ["Swanky and Moo Moo", "cursive"],
+      },
+    },
   },
   plugins: [],
 };
